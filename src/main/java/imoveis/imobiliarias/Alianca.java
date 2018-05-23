f05def63681e2526429c8b2001a8a2a07db62ceb
Ingmar Aguiar <ingmar.aguiar@senior.com.br>
2018-05-23 16:02:20 -0300

    Alianca

--- src/main/java/imoveis/imobiliarias/Alianca.java
+++ src/main/java/imoveis/imobiliarias/Alianca.java
@@ -21,8 +21,8 @@ import imoveis.utils.Utils;
 
 public class Alianca extends ImobiliariaHtml {
 
-    private static final String URLBASE = "http://www.alianca.imb.br/";
-    private static final String PESQUISA = URLBASE + "pesquisa-de-imoveis";
+    private static final String URLBASE = "http://www.alianca.imb.br";
+    private static final String PESQUISA = URLBASE + "/pesquisa-de-imoveis";
 
     public Alianca(String tipo) {
         super(tipo);
@@ -92,18 +92,19 @@ public class Alianca extends ImobiliariaHtml {
 
         @Override
         public void carregarNome() {
-            setNome(elemento.select("a.imovel-item-link").first().text().replace("BLUMENAU / SC", "").replace("BAIRRO", "").trim());
+            Element link = elemento.select("a.localizacao").first();
+            setNome(link.text().replace("Bairro:", "").trim());
+            setUrl(URLBASE.concat(link.attr("href")));
+            setBairro(getNome());
         }
 
         @Override
         public void carregarUrl() {
-            Element link = elemento.select("a.imovel-item-link").first();
-            setUrl(link.attr("href"));
         }
 
         @Override
         public void carregarPreco() {
-            setPrecoStr(elemento.select("div.imovel-item-preco").first().text().replace("Locação:", "").replace("R$", "").trim());
+            setPrecoStr(elemento.select("span.imovel_valor").first().text().replace("R$", "").trim());
             try {
                 setPreco(textoParaReal(getPrecoStr()));
             } catch (Exception e) {
@@ -113,46 +114,39 @@ public class Alianca extends ImobiliariaHtml {
 
         @Override
         public void carregarBairro() {
-            setBairro(elemento.select("div.imovel-item-endereco").first().text().replace("BLUMENAU / SC", "").replace("BAIRRO", "").trim());
         }
 
         @Override
         public void carregarQuartos() {
-            Document documento = getDocumento();
-            Elements dados = documento.select("div.imovel-detalhe-conteudo-texto p");
-            if (dados.size() == 2) {
-                Element last = dados.last();
-                String[] linhas = last.text().toUpperCase().split("»");
-                for (String linha : linhas) {
-                    linha = linha.trim();
-                    if (linha.length() > 1) {
-                        String[] quebrado = linha.split(" ");
-                        if (linha.contains("DORMITÓRIO")) {
-                            setQuartos(Integer.valueOf(quebrado[0].trim()));
-                            if (linha.contains("SUÍTE")) {
-                                int i = 0;
-                                for (String valor : quebrado) {
-                                    if (valor.contains("SUÍTE")) {
-                                        setSuites(Integer.valueOf(quebrado[i - 1].trim()));
-                                    }
-                                    i++;
-                                }
-                            }
-                        }
-                        if (linha.contains("GARAGE")) {
-                            setVagas(Integer.valueOf(quebrado[0].trim()));
-                        }
-                    }
+            Element dado = elemento.select("a.quartos").first();
+            if (dado != null) {
+                String valor = dado.text().trim();
+                if (NumberUtils.isCreatable(valor)) {
+                    setQuartos(Integer.valueOf(valor));
                 }
             }
         }
 
         @Override
         public void carregarVagas() {
+            Element dado = elemento.select("a.garagens").first();
+            if (dado != null) {
+                String valor = dado.text().trim();
+                if (NumberUtils.isCreatable(valor)) {
+                    setVagas(Integer.valueOf(valor));
+                }
+            }
         }
 
         @Override
         public void carregarSuites() {
+            Element dado = elemento.select("a.suites").first();
+            if (dado != null) {
+                String valor = dado.text().trim();
+                if (NumberUtils.isCreatable(valor)) {
+                    setSuites(Integer.valueOf(valor));
+                }
+            }
         }
 
         @Override
@@ -161,7 +155,7 @@ public class Alianca extends ImobiliariaHtml {
 
         @Override
         public void carregarAnunciante() {
-            setAnunciante("Conexão");
+            setAnunciante("Aliança");
         }
 
         @Override
