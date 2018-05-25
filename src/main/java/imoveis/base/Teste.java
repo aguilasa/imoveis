7c33fdce66bbcb857366636efef31734827d2e31
Ingmar Aguiar <ingmar.aguiar@senior.com.br>
2018-05-25 18:12:40 -0300

    Habitação

--- src/main/java/imoveis/base/Teste.java
+++ src/main/java/imoveis/base/Teste.java
@@ -21,25 +21,16 @@ public class Teste {
     }
 
     public void run() throws Exception {
+
         try (HttpClientHelper helper = new HttpClientHelper()) {
             List<NameValuePair> params = new ArrayList<>();
-            params.add(new BasicNameValuePair("codTB", "1"));
-            params.add(new BasicNameValuePair("codUF", "0"));
-            params.add(new BasicNameValuePair("codCid", "11"));
-            params.add(new BasicNameValuePair("codBai", "0"));
-            params.add(new BasicNameValuePair("codTP", "2"));
-            params.add(new BasicNameValuePair("qtdQuartos", "0"));
-            params.add(new BasicNameValuePair("qtdSuites", "0"));
-            params.add(new BasicNameValuePair("qtdSalas", "0"));
-            params.add(new BasicNameValuePair("qtdGaragens", "0"));
-            params.add(new BasicNameValuePair("codVal", "Min"));
-            params.add(new BasicNameValuePair("codValBK", "0"));
-            params.add(new BasicNameValuePair("codVal2", "Max"));
-            params.add(new BasicNameValuePair("codValbk2", "0"));
-            params.add(new BasicNameValuePair("searchtype", "2"));
-            params.add(new BasicNameValuePair("codOrd", "1"));
-            params.add(new BasicNameValuePair("pageIndex", "0"));
-            HttpPost httpPost = helper.httpPost("http://www.alianca.imb.br/pesquisa-de-imoveis", params);
+            params.add(new BasicNameValuePair("negocio_", "2"));
+            params.add(new BasicNameValuePair("tipo_", "5"));
+            params.add(new BasicNameValuePair("cidade_", "1"));
+            params.add(new BasicNameValuePair("bairro_", ""));
+            params.add(new BasicNameValuePair("quartos_", ""));
+            params.add(new BasicNameValuePair("pagina", "2"));
+            HttpPost httpPost = helper.httpPost("http://www.habitacaoimob.com.br/action-busca.php", params);
             System.out.println(helper.execute(httpPost));
         }
     }
--- src/main/java/imoveis/imobiliarias/Habitacao.java
+++ src/main/java/imoveis/imobiliarias/Habitacao.java
@@ -2,17 +2,20 @@ package imoveis.imobiliarias;
 
 import static imoveis.utils.Utils.textoParaReal;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONObject;
-import org.jsoup.nodes.Comment;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
-import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 import imoveis.base.IImovel;
@@ -20,22 +23,48 @@ import imoveis.base.Imobiliaria;
 import imoveis.base.ImobiliariaHtml;
 import imoveis.base.ImovelHtml;
 import imoveis.excel.Excel;
+import imoveis.utils.HttpClientHelper;
 import imoveis.utils.Utils;
 
 public class Habitacao extends ImobiliariaHtml {
 
-    private static final String URLBASE = "http://www.alianca.imb.br";
-    private static final String PESQUISA = URLBASE + "/pesquisa-de-imoveis";
+    private static final String URLBASE = "http://www.habitacaoimob.com.br/";
+    private static final String PESQUISA = URLBASE + "action-busca.php";
 
     public Habitacao(String tipo) {
         super(tipo);
-        setPost(true);
+    }
+    
+    @Override
+    public Document getDocument() {
+        return getDocument(getUrl());
+    }
+
+    @Override
+    public Document getDocument(String url) {
+        try (HttpClientHelper helper = new HttpClientHelper()) {
+            
+            Map<String, String> payload = getPayload();
+            Iterator<String> iterator = payload.keySet().iterator();
+            List<NameValuePair> params = new ArrayList<>();
+            while(iterator.hasNext()) {
+                String key = iterator.next();
+                String value = payload.get(key);
+                params.add(new BasicNameValuePair(key, value));
+            }
+            
+            HttpPost httpPost = helper.httpPost(url, params);
+            String html = helper.execute(httpPost);
+            return Jsoup.parse(html);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Elements getElementos() {
         Document document = getDocument(getUrl());
-        return document.select("div#imoveis_grid div.imovel_bloco_voosuave");
+        return document.select("div.content-resultado-busca ul a");
     }
 
     @Override
@@ -46,7 +75,7 @@ public class Habitacao extends ImobiliariaHtml {
     @Override
     public int getPaginas() {
         Document document = getDocument();
-        Elements elements = document.select("div#paginationTopContent div.pagination a");
+        Elements elements = document.select("div#paginacao a");
         if (!elements.isEmpty()) {
             int p = 0;
             for (Element e : elements) {
@@ -63,22 +92,12 @@ public class Habitacao extends ImobiliariaHtml {
     @Override
     public Map<String, String> getPayload() {
         LinkedHashMap<String, String> payload = new LinkedHashMap<>();
-        payload.put("codTB", "1");
-        payload.put("codUF", "0");
-        payload.put("codCid", "11");
-        payload.put("codBai", "0");
-        payload.put("codTP", tipo.equals("apartamento") ? "2" : "1");
-        payload.put("qtdQuartos", "0");
-        payload.put("qtdSuites", "0");
-        payload.put("qtdSalas", "0");
-        payload.put("qtdGaragens", "0");
-        payload.put("codVal", "Min");
-        payload.put("codValBK", "0");
-        payload.put("codVal2", "Max");
-        payload.put("codValbk2", "0");
-        payload.put("searchtype", "2");
-        payload.put("codOrd", "1");
-        payload.put("pageIndex", String.valueOf(pagina - 1));
+        payload.put("negocio_", "2");
+        payload.put("tipo_", tipo.equals("apartamento") ? "5" : "4");
+        payload.put("cidade_", "1");
+        payload.put("bairro_", "");
+        payload.put("quartos_", "");
+        payload.put("pagina", String.valueOf(pagina));
         return payload;
     }
 
@@ -95,19 +114,18 @@ public class Habitacao extends ImobiliariaHtml {
 
         @Override
         public void carregarNome() {
-            Element link = elemento.select("a.localizacao").first();
-            setNome(link.text().replace("Bairro:", "").trim());
-            setUrl(URLBASE.concat(link.attr("href")));
-            setBairro(getNome());
+            Element dados = elemento.select("div.txt-resultado-busca").first();
+            setNome(dados.text().trim());
         }
 
         @Override
         public void carregarUrl() {
+            setUrl(URLBASE.concat(elemento.attr("href")));
         }
 
         @Override
         public void carregarPreco() {
-            setPrecoStr(elemento.select("span.imovel_valor").first().text().replace("R$", "").trim());
+            setPrecoStr(elemento.select("div.preco-busca").first().text().replace("R$", "").trim());
             try {
                 setPreco(textoParaReal(getPrecoStr()));
             } catch (Exception e) {
@@ -117,39 +135,24 @@ public class Habitacao extends ImobiliariaHtml {
 
         @Override
         public void carregarBairro() {
+            Elements dados = elemento.select("div.caixas-busca");
+            if (dados.size() >= 4) {
+                setBairro(dados.get(2).text().trim());
+                String valor = dados.get(3).text().split(":")[1].trim();
+                setQuartos(Integer.valueOf(valor));
+            }
         }
 
         @Override
         public void carregarQuartos() {
-            Element dado = elemento.select("a.quartos").first();
-            if (dado != null) {
-                String valor = dado.text().trim();
-                if (NumberUtils.isCreatable(valor)) {
-                    setQuartos(Integer.valueOf(valor));
-                }
-            }
         }
 
         @Override
         public void carregarVagas() {
-            Element dado = elemento.select("a.garagens").first();
-            if (dado != null) {
-                String valor = dado.text().trim();
-                if (NumberUtils.isCreatable(valor)) {
-                    setVagas(Integer.valueOf(valor));
-                }
-            }
         }
 
         @Override
         public void carregarSuites() {
-            Element dado = elemento.select("a.suites").first();
-            if (dado != null) {
-                String valor = dado.text().trim();
-                if (NumberUtils.isCreatable(valor)) {
-                    setSuites(Integer.valueOf(valor));
-                }
-            }
         }
 
         @Override
@@ -158,48 +161,36 @@ public class Habitacao extends ImobiliariaHtml {
 
         @Override
         public void carregarAnunciante() {
-            setAnunciante("Aliança");
+            setAnunciante("Habitação");
         }
 
         @Override
         public void carregarCondominio() {
             Document documento = getDocumento();
-            Elements dados = documento.select("b.preco_m2");
+            Elements dados = documento.select("div.content-imoveis-prontos-det ul li");
             for (Element dado : dados) {
-                Element anterior = dado.previousElementSibling();
-                if (anterior != null && anterior.text().toLowerCase().contains("condomínio")) {
-                    String valor = dado.text().toLowerCase().trim();
-                    valor = valor.replace(".", "").replace(",", ".").trim();
-                    setCondominio(Double.valueOf(valor));
+                String valor = dado.text().toLowerCase().trim();
+                String[] separado = valor.split(":");
+                if (separado.length == 2) {
+                    if (valor.contains("condomínio")) {
+                        valor = separado[1].trim();
+                        setCondominio(textoParaReal(valor));
+                    } else if (valor.contains("garagem")) {
+                        setVagas(Integer.valueOf(separado[1].trim()));
+                    }
+
                 }
             }
         }
 
         @Override
         public void carregarEndereco() {
-            Document documento = getDocumento();
-            Elements dados = documento.select("div.panel-body p b");
-            for (Element dado : dados) {
-                String valor = dado.text().toLowerCase().trim();
-                if (valor.contains("localiza")) {
-                    StringBuilder localizacao = new StringBuilder();
-                    Node proximo = dado.nextSibling();
-                    while (proximo != null && !(proximo instanceof Comment)) {
-                        if (proximo instanceof TextNode) {
-                            TextNode text = (TextNode) proximo;
-                            localizacao.append(text.text().trim());
-                        }
-                        proximo = proximo.nextSibling();
-                    }
-                    setEndereco(localizacao.toString().trim());
-                }
-            }
         }
 
     }
 
     public static void main(String[] args) {
-        Imobiliaria imobiliaria = new Habitacao("casa");
+        Imobiliaria imobiliaria = new Habitacao("apartamento");
         List<IImovel> imos = imobiliaria.getImoveis();
         Excel.getInstance().clear();
         for (IImovel imo : imos) {
